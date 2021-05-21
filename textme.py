5c61bd37c32dc6df32e8bcc1f097a80fac0aa2a5
Prajwol Chhetri <71620452+Prajwol-Chhetri@users.noreply.github.com>
2021-05-21 17:31:25 +0545

    Update textme.py

--- textme.py
+++ textme.py
@@ -6,7 +6,7 @@ auth_token = ''
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create(  
-                              messaging_service_sid='MG564fd6ec84f8b023f15c307a85e573ee',
+                              messaging_service_sid='', # enter sid here
                               body='Hello from Message Bot',   # enter your message here      
                               to='' #enter your number here
                           ) 
