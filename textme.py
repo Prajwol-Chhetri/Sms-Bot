6ab1c84bdea922df0b5dcb46540a40566acc5120
Prajwol Chhetri <prajwalc391@gmail.com>
2021-05-21 16:58:49 +0545

    removed personal sid, auth token, number for privacy

--- textme.py
+++ textme.py
@@ -1,13 +1,14 @@
 from twilio.rest import Client 
  
-account_sid = 'AC84aab39541e43904c5dfe40abc79818d' 
-auth_token = '019e354d0170ed2b9b249cb3478b8ae0' 
+#enter account sid and auth token provided from twilio 
+account_sid = '' 
+auth_token = ''  
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create(  
                               messaging_service_sid='MG564fd6ec84f8b023f15c307a85e573ee',
-                              body='Hello from Message Bot',         
-                              to='+9779800515353' 
+                              body='Hello from Message Bot',   # enter your message here      
+                              to='' #enter your number here
                           ) 
  
 print(message.sid)
